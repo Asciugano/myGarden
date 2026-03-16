520b5654c1f23eef8ca7ef3ed328a96bab33a5db
Asciugano <diaferiosamuele@gmail.com>
2026-03-16 18:11:28 +0100

    aggiunto un run.sh per lavorare dal terminale

--- src/main/java/com/asciugano/game/entity/tiles/TerrainTile.java
+++ src/main/java/com/asciugano/game/entity/tiles/TerrainTile.java
@@ -2,34 +2,35 @@ package com.asciugano.game.entity.tiles;
 
 import com.asciugano.engine.models.ColoredModel;
 import com.asciugano.engine.models.MeshBuilder;
-import com.asciugano.engine.models.RawModel;
 import com.asciugano.engine.renderer.Loader;
 import com.asciugano.engine.utils.Color;
 import org.joml.Vector3f;
 
 public abstract class TerrainTile extends Tile {
-    protected ColoredModel model;
+  protected ColoredModel model;
 
-    public TerrainTile(Tile tile, Loader loader) {
-        super(tile.getGridX(), tile.getGridZ());
-        MeshBuilder meshBuilder = new MeshBuilder();
+  public TerrainTile(Tile tile, Loader loader) {
+    super(tile.getGridX(), tile.getGridZ());
+    MeshBuilder meshBuilder = new MeshBuilder();
 
-        generateMeshVertices(meshBuilder);
-        this.model = new ColoredModel(
-                loader.loadToVAO(
-                        meshBuilder.getVertices(),
-                        meshBuilder.getColors(),
-                        3,
-                        meshBuilder.getNormals(),
-                        meshBuilder.getIndices()
-                ),
-                new Color(new Vector3f(meshBuilder.getColors()))
-        );
-    }
+    generateMeshVertices(meshBuilder);
+    this.model = new ColoredModel(
+        loader.loadToVAO(
+            meshBuilder.getVertices(),
+            meshBuilder.getColors(),
+            3,
+            meshBuilder.getNormals(),
+            meshBuilder.getIndices()),
+        new Color(new Vector3f(meshBuilder.getColors())));
+  }
 
-    public ColoredModel getModel() { return model; }
+  public ColoredModel getModel() {
+    return model;
+  }
 
-    public abstract float getEdgeHeight();
-    public abstract Color getEdgeColor();
-    protected abstract void generateMeshVertices(MeshBuilder meshBuilder);
+  public abstract float getEdgeHeight();
+
+  public abstract Color getEdgeColor();
+
+  protected abstract void generateMeshVertices(MeshBuilder meshBuilder);
 }
